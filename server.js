5449243886f5db3ae105a8959b44a0533f31b177
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-10 12:49:25 +0200

    sending a response

--- server.js
+++ server.js
@@ -4,4 +4,13 @@ const PORT = 8000
 
 const app = express()
 
+const trainees = {
+    name: 'hope',
+    age: 24
+}
+
+app.get('/', (req, res) =>{
+    res.json(trainees)
+})
+
 app.listen(PORT, () => console.log('the site is connected to port: ' + PORT))
