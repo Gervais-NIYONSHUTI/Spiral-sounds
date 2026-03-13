36ea5b929b0b5d775b5b9f8f83f258821f4dbfc8
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-13 09:28:20 +0200

    removed unnecessary files

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,11 +1,18 @@
-export async function getGenres(){
+import { db } from "../db/dbConnection.js"
 
-  console.log('genres')
+export async function getGenres(req, res){
 
+  const genres = db.prepare('select distinct(genre) from products')
+  const genresRows = genres.all().map(val => val.genre)
+  res.json(genresRows)
 }
 
-export async function getProducts(){
-
-  console.log('products')
+export async function getProducts(req, res){
+  const { genre } = req.query
+  console.log(genre)
+  let genres = ''
+  if(genre !== undefined) genres = db.prepare(`select * from products where genre = '${genre}'`)
+  else genres = db.prepare('select * from products')
+  res.json(genres.all())
 
 }
