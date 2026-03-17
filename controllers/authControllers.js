5a8425d61f2ec9fcf0543dba7e21bd17fc2f9d76
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-17 12:42:47 +0200

    new data added to the database

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,25 @@
+import validator from 'validator'
+import { db } from '../db/dbConnection.js'
+
 export async function registerUser(req, res){
-    console.log(req.body)
+    let { name, email, username, password} = req.body
+    if( !name || !email || !username || !password){
+        res.status(400).json({error: "All fields must be completed"})
+    }
+    name = name.trim()
+    email = email.trim()
+    username = username.trim()
+    if(!/^[a-zA-Z0-9_-]{1,20}$/.test(username)){
+        res.status(400).json({error: "Username must be 1–20 characters, using letters, numbers, _ or -."})
+    }
+    if(!validator.isEmail(email)){
+        res.status(400).json({error: "Invaid email format"})
+    }
+    const users = db.prepare('SELECT id FROM users WHERE email = ? OR username = ?').all(name, email)
+    if(users.length){
+        return res.status(400).json({ error: 'Email or username already in use.' })
+    } else {
+        db.prepare("Insert into users (name, email, username, password) values(?,?,?,?)").run(name, email, username, password)
+        res.status(201).json({ message: 'User registered'})
+    }
 }
--- logTable.js
+++ logTable.js
@@ -1,7 +1,6 @@
-import Database from 'better-sqlite3'
+import { db } from './db/dbConnection.js'
 
 // async function viewAllProducts(){
-    const db = new Database('database.db')
     const select = db.prepare('Select* from users')
     console.table(select.all())
 // }
