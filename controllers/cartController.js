f7a7f0ce361da4adeee6bcf18b3b08f158d90352
Gervais NIYONSHUTI <gervaisniyonshuti@gmail.com>
2026-03-30 09:08:40 +0200

    made cart products visible in checkout

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -18,3 +18,12 @@ export async function getCartCount(req, res) {
   const result = db.prepare(`SELECT SUM(quantity) AS totalItems FROM cart_items WHERE user_id = ?`).get(req.session.userID)
   res.json({ totalItems: result.totalItems || 0 })
 }
+
+export async function getAll(req, res) {
+    if(!req.session.userID) {
+        return res.json({err: "not logged in"})
+    }
+    const result = db.prepare(`SELECT ci.id AS cartItemId, ci.quantity, p.title, p.artist, p.price FROM cart_items ci JOIN products p ON p.id = ci.product_id WHERE ci.user_id = ?`).all(req.session.userID)
+    console.log(result)
+    res.json({items: result})
+}
--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,8 @@
-import { addToCart, getCartCount } from "../controllers/cartController.js";
+import { addToCart, getCartCount, getAll } from "../controllers/cartController.js";
 import express from "express";
 
 export const cartRouter = express.Router()
 
 cartRouter.post('/add', addToCart)
 cartRouter.get('/cart-count', getCartCount)
+cartRouter.get('/', getAll)
